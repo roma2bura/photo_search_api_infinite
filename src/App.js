5594b9e62e2623ccb7a7da495c61eb41282aaa7e
Roman Bura <r2bcloud@gmail.com>
2021-06-14 19:57:41 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("lamborghini");
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
+  const client_id = "yS4ofzOSRLDZbfolYUAoI_b7I8INeEAERlgdDx_ewEQ";
+  const fetchUrl = `https://api.unsplash.com/search/photos?client_id=${client_id}&query=${query}&page=${page}`;
+
+  const fetchImages = () => {
+    axios
+      .get(fetchUrl, {
+        headers: {},
+      })
+      .then((response) => {
+        setData([...data, ...response.data.results]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setPage(page + 1);
+  };
+  const searchImages = (e) => {
+    if (e.keyCode === 13) {
+      setQuery(e.target.value);
+      setData([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+
+  <div className="form">
+      <input
+        type="text"
+        className="input"
+        onKeyDown={(e) => searchImages(e)}
+        placeholder="Search For Images 🔎"
+      />
+</div>
+      
+      <InfiniteScroll
+        dataLength={data.length}
+        next={fetchImages}
+        hasMore={hasMore}
+        loader={<p>Load more...</p>}
+        endMessage={
+          <p style={{ textAlign: "center" }}> 
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        
+        <div className="card-list">
+          {data.map((data, key) => (
+            <div className="container" key={key}>
+              <img
+                src={data.urls.small}
+                className="card--image"
+                alt={data.alt_description}
+              />
+              <h4>Photo by {data.user.name} 📸</h4>
+            </div>
+          ))}
+        </div>
+      </InfiniteScroll>
+    </>
   );
 }
 
